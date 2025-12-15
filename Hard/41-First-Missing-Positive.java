3bcf30ae0edd81e90541c9625df5effa9bc5186d
works <zjs.suarez@gmail.com>
2025-12-15 10:57:17 +0100

    Solved Hard problem number 41 "First Missing Positive"

---
+++ Hard/41-First-Missing-Positive.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        int num = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]>0){
+                if(nums[i]==num) num++;
+            }
+        }
+
+        return num;
+    }
+}
