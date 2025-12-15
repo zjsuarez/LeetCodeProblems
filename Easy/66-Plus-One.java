e7d1808e87091558b25e4084059b15ed171332de
works <zjs.suarez@gmail.com>
2025-12-15 10:02:58 +0100

    Optimized 66-Plus-One exercise to reach for 0ms runtime

--- Easy/66-Plus-One.java
+++ Easy/66-Plus-One.java
@@ -1,45 +1,24 @@
-
-
 class Solution {
     public int[] plusOne(int[] digits) {
-
-
-        int [] tabla;
         boolean allnines = true;
-
         for (int i = 0; i < digits.length; i++) {
             if(digits[i]!=9){
                 allnines=false;
             }
         }
-
         if (allnines) {
-            tabla = new int[digits.length+1];
-            tabla[0]=1;
-            for (int i = 1; i < tabla.length; i++) {
-                tabla[i]=0;
-            }
+            digits = new int[digits.length+1];
+            digits[0]=1;
         } else{
-            tabla=new int[digits.length];
-
-            for (int i = 0; i < tabla.length; i++) {
-                tabla[i]=digits[i];
-            }
-
-            tabla[digits.length-1]+=1;
-            for (int j = 0; j < tabla.length; j++) {
-                for (int i = 0; i < tabla.length; i++) {
-                    if (tabla[i]==10) {
-                        tabla[i]=0;
-                        tabla[i-1]+=1;
-                    }
+            digits[digits.length-1]+=1;
+            if(digits[digits.length-1]==10){
+                for (int i = 1; i < digits.length; i++) {
+                    digits[digits.length-1-i]+=digits[digits.length-i]/10;
+                    if(digits[digits.length-i]==10) digits[digits.length-i]=0;
                 }
             }
         }
-
-
-
-        return tabla;
+        return digits;
 
     }
 }
