b8d6e402adb1daf62bffe6b5cfb44622144faa55
works <zjs.suarez@gmail.com>
2025-12-17 11:37:12 +0100

    Reduced Runtime ms from 53ms to 10ms in Medium problem number 6 "Zigzag Conversion"

--- Medium/6-Zigzag-Conversion.java
+++ Medium/6-Zigzag-Conversion.java
@@ -1,35 +1,38 @@
 class Solution {
     public String convert(String s, int numRows) {
-        if(s.length()<3) return s;
 
-        String [][] arr = new String[numRows][s.length()];
-        int pos = 0;
-        int col = 0;
+        List<String> [] arr = new ArrayList[numRows];
+        for (int i = 0; i < numRows; i++) {
+            arr[i] = new ArrayList<>();
+        }
 
         String res = "";
+
+        int pos = 0;
 
         loop1:
         while(pos<s.length()){
             for (int i = 0; i < numRows; i++) {
-                arr[i][col] = ""+s.charAt(pos);
+                arr[i].add(""+s.charAt(pos));
                 pos++;
                 if(pos>=s.length()) break loop1;
             }
+
             int row = numRows-2;
-            col++;
             while(row<=numRows-2 && row>= 1){
-                arr[row][col] = ""+s.charAt(pos);
+                arr[row].add(""+s.charAt(pos));
                 pos++;
                 if(pos>=s.length()) break loop1;
                 row--;
-                col++;
             }
         }
+
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[0].length; j++) {
-                if(arr[i][j]!=null) res= res+arr[i][j];
+            for (int j = 0; j < arr[i].size(); j++) {
+                res= res+arr[i].get(j);
             }
         }
+
         return res;
     }
 }
