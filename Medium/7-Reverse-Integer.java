d1c25d0af31251c7f98553917638f09acdbcf3fb
works <zjs.suarez@gmail.com>
2025-12-17 09:38:55 +0100

    Cleaned Medium problem number 7 "Reverse Integer"

--- Medium/7-Reverse-Integer.java
+++ Medium/7-Reverse-Integer.java
@@ -3,27 +3,16 @@ class Solution {
         Integer num = new Integer(x);
         StringBuilder str = new StringBuilder(num.toString());
 
-        int pos = str.length()-1;
-        while(true){
-            if(str.charAt(pos)=='0' && x!=0){
-                str.deleteCharAt(pos);
-                pos--;
-            } else{
-                break;
-            }
-        }
         str.reverse();
 
         if(x<0){
             str.deleteCharAt(str.length()-1);
-            try {
-                return Integer.parseInt(str.toString())*-1;
-            } catch (NumberFormatException e){
-                return 0;
-            }
         }
 
         try {
+            if(x<0){
+                return Integer.parseInt(str.toString())*-1;
+            }
             return Integer.parseInt(str.toString());
         } catch (NumberFormatException e){
             return 0;
