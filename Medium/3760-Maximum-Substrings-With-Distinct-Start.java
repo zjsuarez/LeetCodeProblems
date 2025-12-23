57e482a23701ebf1542125c4fe419ddd03de0818
works <zjs.suarez@gmail.com>
2025-12-23 11:37:52 +0100

    Reduced runtime from 121ms to 31ms in medium problem number 3760 "Maximum Substrings With Distinct Start".

--- Medium/3760-Maximum-Substrings-With-Distinct-Start.java
+++ Medium/3760-Maximum-Substrings-With-Distinct-Start.java
@@ -2,13 +2,10 @@ class Solution {
     public int maxDistinct(String s) {
         int count = 0;
 
-        Map<Integer,Character> map = new HashMap<>();
+        Map<Character,Integer> map = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
-            if(!map.containsValue(s.charAt(i))){
-                map.put(map.size(),s.charAt(i));
-                count++;
-            }
+            m
         }
 
 
