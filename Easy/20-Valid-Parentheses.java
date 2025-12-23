3a0ab47fa79633910d4182edfd0e2b3ed31b3add
works <zjs.suarez@gmail.com>
2025-12-23 10:27:58 +0100

    Reduced runtime from 7ms to 6ms xd in Easy problem number 20 "Valid Parentheses".

--- Easy/20-Valid-Parentheses.java
+++ Easy/20-Valid-Parentheses.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length()%2!=0) return false;
         List<Character> stack = new ArrayList<>();
         Map <Character, Character> opening = new HashMap<>();
         opening.put(')','(');
